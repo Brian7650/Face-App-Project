006c55ae9de8d5def145538a52b3fedfc6474e7a
Brian B <brtl567@gmail.com>
2021-12-17 14:25:32 -0500

    frontend joining the club

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://aqueous-garden-34137.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
