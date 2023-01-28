77edd7ef96e8f64c459ca3888b092e90792848e5
Brian.B <brtl765@gmail.com>
2023-01-27 19:11:03 -0500

    coweb particlebg type

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ onRouteChange = (route) => {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
     <div className="App">
-     <ParticlesBg type="circle" bg={true} />
+     <ParticlesBg type="cobweb" bg={true} />
       <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
       { route === 'home'
         ? <div>
