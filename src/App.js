6e775f45b635efa7b7c5d04172a7a3c0dddd1b87
Brian.B <brtl765@gmail.com>
2023-01-27 20:35:20 -0500

    square particlebg type

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ onRouteChange = (route) => {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
     <div className="App">
-     <ParticlesBg type="color" bg={true} />
+     <ParticlesBg type="square" bg={true} />
       <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
       { route === 'home'
         ? <div>
