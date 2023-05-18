4e249ed61647c4e7df254b3ff3867a837c032e0d
Brian.B <brtl765@gmail.com>
2023-05-18 13:41:22 -0400

    addeed form in imageLinkForm

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,6 +4,7 @@ import './ImageLinkForm.css';
 const ImageLinkForm= ({ onInputChange, onButtonSubmit }) => {
   return (
   <div>
+    <form>
     <p className='f2'>
     {'This Magic Brain will detect faces in your pictures. Give it a try.'} 
     </p>
@@ -16,6 +17,7 @@ const ImageLinkForm= ({ onInputChange, onButtonSubmit }) => {
         >Detect</button>
       </div>
     </div>
+    </form>
   </div>
   );
 }
