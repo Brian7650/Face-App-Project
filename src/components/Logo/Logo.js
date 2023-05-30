aab47ca183787ba027068959be8c83cfc0f37a9a
Brian.B <brtl765@gmail.com>
2023-05-30 09:28:34 -0400

    add new logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
-import brain from './brain.png';
+import face from './face.png';
 import './Logo.css';
 
 const Logo= () => {
 	return (
 	<div className='ma4 mt0'>
 		<Tilt className="Tilt br2 shadow-1" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
- 		<div className="Tilt-inner pa3"><img style={{paddingTop: '5px'}} alt='brain logo' src={brain}/> </div>
+ 		<div className="Tilt-inner pa3"><img style={{paddingTop: '5px'}} alt='face logo' src={face}/> </div>
 		</Tilt>
 	</div>
   );
